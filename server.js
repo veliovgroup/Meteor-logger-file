6f97a4ebe621b2d13e1665f943589659b921e2ef
dr.dimitru <ceo@veliov.com>
2018-01-02 12:59:34 +0300

    Fix #11 , thanks to @blueyestar

--- server.js
+++ server.js
@@ -41,67 +41,67 @@ class LoggerFile {
 
         return `${date}-${month}-${year}.log`;
       };
-
-      /* format - Log record format */
-      if (this.options.format) {
-        if(!_.isFunction(this.options.format)) {
-          throw new Meteor.Error('[LoggerFile] [options.format] Must be a Function!');
-        }
-      } else {
-        this.options.format = (time, level, message, data, userId) => {
-          let month = `${time.getMonth() + 1}`;
-          if (month.length === 1) {
-            month = '0' + month;
-          }
-          let date  = `${time.getDate()}`;
-          if (date.length === 1) {
-            date  = '0' + date;
-          }
-          let year  = `${time.getFullYear()}`;
-          if (year.length === 1) {
-            year  = '0' + year;
-          }
-          let hours = `${time.getHours()}`;
-          if (hours.length === 1) {
-            hours = '0' + hours;
-          }
-          let mins  = `${time.getMinutes()}`;
-          if (mins.length === 1) {
-            mins  = '0' + mins;
-          }
-          let sec   = `${time.getSeconds()}`;
-          if (sec.length === 1) {
-            sec   = '0' + sec;
-          }
-
-          return `${date}-${month}-${year} ${hours}:${mins}:${sec} | [${level}] | Message: \"${message}\" | User: ${userId} | data: ${data}\r\n`;
-        };
-      }
-
-      /* path - Log's storage path */
-      if (this.options.path) {
-        if (!_.isString(this.options.path)) {
-          throw new Meteor.Error('[LoggerFile] [options.path] Must be a String!');
-        }
-      } else {
-        this.options.path = Meteor.rootPath + ((process.env.NODE_ENV === 'development') ? '/static/logs' : '/assets/app/logs');
-      }
-
-      this.options.path = this.options.path.replace(/\/$/, '');
-
-      fs.ensureDir(`${this.options.path}`, (EDError) => {
-        if (EDError) {
-          throw new Meteor.Error('[LoggerFile] [options.path] Error:', EDError);
-        }
-
-        fs.writeFile(`${this.options.path}/test`, 'test', (WFError) => {
-          if (WFError) {
-            throw new Meteor.Error(`[LoggerFile] [options.path] ${this.options.path} is not writable!!!`, WFError);
-          }
-          fs.unlink(`${this.options.path}/test`, NOOP);
-        });
-      });
     }
+
+    /* format - Log record format */
+    if (this.options.format) {
+      if(!_.isFunction(this.options.format)) {
+        throw new Meteor.Error('[LoggerFile] [options.format] Must be a Function!');
+      }
+    } else {
+      this.options.format = (time, level, message, data, userId) => {
+        let month = `${time.getMonth() + 1}`;
+        if (month.length === 1) {
+          month = '0' + month;
+        }
+        let date  = `${time.getDate()}`;
+        if (date.length === 1) {
+          date  = '0' + date;
+        }
+        let year  = `${time.getFullYear()}`;
+        if (year.length === 1) {
+          year  = '0' + year;
+        }
+        let hours = `${time.getHours()}`;
+        if (hours.length === 1) {
+          hours = '0' + hours;
+        }
+        let mins  = `${time.getMinutes()}`;
+        if (mins.length === 1) {
+          mins  = '0' + mins;
+        }
+        let sec   = `${time.getSeconds()}`;
+        if (sec.length === 1) {
+          sec   = '0' + sec;
+        }
+
+        return `${date}-${month}-${year} ${hours}:${mins}:${sec} | [${level}] | Message: \"${message}\" | User: ${userId} | data: ${data}\r\n`;
+      };
+    }
+
+    /* path - Log's storage path */
+    if (this.options.path) {
+      if (!_.isString(this.options.path)) {
+        throw new Meteor.Error('[LoggerFile] [options.path] Must be a String!');
+      }
+    } else {
+      this.options.path = Meteor.rootPath + ((process.env.NODE_ENV === 'development') ? '/static/logs' : '/assets/app/logs');
+    }
+
+    this.options.path = this.options.path.replace(/\/$/, '');
+
+    fs.ensureDir(`${this.options.path}`, (EDError) => {
+      if (EDError) {
+        throw new Meteor.Error('[LoggerFile] [options.path] Error:', EDError);
+      }
+
+      fs.writeFile(`${this.options.path}/test`, 'test', (WFError) => {
+        if (WFError) {
+          throw new Meteor.Error(`[LoggerFile] [options.path] ${this.options.path} is not writable!!!`, WFError);
+        }
+        fs.unlink(`${this.options.path}/test`, NOOP);
+      });
+    });
 
     this.logger.add('File', (level, message, data = null, userId) => {
       const time = new Date();
