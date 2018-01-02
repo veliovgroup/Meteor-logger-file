1cbb93b1921f8ee5a33cc0c01cc8a131ea0e6130
dr.dimitru <ceo@veliov.com>
2018-01-02 13:00:15 +0300

    v2.0.4
     - NPM and Atmosphere dependencies update
     - Fix #11 , thanks to @blueyestar

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:loggerfile',
-  version: '2.0.3',
+  version: '2.0.4',
   summary: 'Logging: Store application\'s logs into file (Server & Client support)',
   git: 'https://github.com/VeliovGroup/Meteor-logger-file',
   documentation: 'README.md'
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.4');
   api.use('ostrio:meteor-root@1.0.6', 'server');
-  api.use(['ecmascript', 'check', 'ostrio:logger@2.0.5'], ['client', 'server']);
+  api.use(['ecmascript', 'check', 'ostrio:logger@2.0.6'], ['client', 'server']);
   api.use('underscore', 'server');
   api.mainModule('client.js', 'client');
   api.mainModule('server.js', 'server');
@@ -17,10 +17,10 @@ Package.onUse(function(api) {
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use(['ecmascript', 'underscore', 'ostrio:logger@2.0.5', 'ostrio:loggerfile@2.0.3']);
+  api.use(['ecmascript', 'underscore', 'ostrio:logger@2.0.6', 'ostrio:loggerfile']);
   api.addFiles('loggerfile-tests.js');
 });
 
 Npm.depends({
-  'fs-extra': '4.0.3'
+  'fs-extra': '5.0.0'
 });
