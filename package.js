b07db20c146a94763d9313caaa529c3bc29e0e96
dr.dimitru <ceo@veliov.com>
2018-07-27 03:20:02 +0300

    v2.0.6
     - 🗑 Get rid of `underscore` package
     - 📦 Update NPM and Atmosphere dependencies
     - 🤝 Compatibility with `meteor@1.7.0.3`

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:loggerfile',
-  version: '2.0.5',
+  version: '2.0.6',
   summary: 'Logging: Store application\'s logs into file (Server & Client support)',
   git: 'https://github.com/VeliovGroup/Meteor-logger-file',
   documentation: 'README.md'
@@ -8,8 +8,8 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.4');
-  api.use('ostrio:meteor-root@1.0.6', 'server');
-  api.use(['ecmascript', 'check', 'ostrio:logger@2.0.6'], ['client', 'server']);
+  api.use('ostrio:meteor-root@1.0.7', 'server');
+  api.use(['ecmascript', 'check', 'ostrio:logger@2.0.7'], ['client', 'server']);
   api.use('underscore', 'server');
   api.mainModule('client.js', 'client');
   api.mainModule('server.js', 'server');
@@ -22,5 +22,5 @@ Package.onTest(function(api) {
 });
 
 Npm.depends({
-  'fs-extra': '5.0.0'
+  'fs-extra': '7.0.0'
 });
