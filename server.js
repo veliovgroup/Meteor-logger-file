e1b054cdbcb44fe97fbf24ceaadea1f839dc6c68
dr.dimitru <ceo@veliov.com>
2022-06-01 08:03:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-import { Meteor }       from 'meteor/meteor';
-import { Logger }       from 'meteor/ostrio:logger';
+import { Meteor } from 'meteor/meteor';
+import { Logger } from 'meteor/ostrio:logger';
 import { check, Match } from 'meteor/check';
-import fs               from 'fs-extra';
-import nodePath         from 'path';
 
-const NOOP = () => {};
+import fs from 'fs';
+import nodePath from 'path';
+
+const noop = () => {};
 
 const helpers = {
   isObject(obj) {
@@ -30,12 +31,12 @@ const helpers = {
 
 const _helpers = ['String'];
 for (let i = 0; i < _helpers.length; i++) {
-  helpers['is' + _helpers[i]] = function (obj) {
-    return Object.prototype.toString.call(obj) === '[object ' + _helpers[i] + ']';
+  helpers[`is${_helpers[i]}`] = function (obj) {
+    return Object.prototype.toString.call(obj) === `[object ${_helpers[i]}]`;
   };
 }
 
-/*
+/**
  * @class LoggerFile
  * @summary File (FS) adapter for ostrio:logger (Logger)
  */
@@ -44,7 +45,7 @@ class LoggerFile {
     check(logger, Match.OneOf(Logger, Object));
     check(options, Match.Optional(Object));
 
-    this.logger  = logger;
+    this.logger = logger;
     this.options = options;
 
     /* fileNameFormat - Log file name */
@@ -59,14 +60,14 @@ class LoggerFile {
           month = '0' + month;
         }
 
-        let date  = `${time.getDate()}`;
+        let date = `${time.getDate()}`;
         if (date.length === 1) {
-          date  = '0' + date;
+          date = '0' + date;
         }
 
-        let year  = `${time.getFullYear()}`;
+        let year = `${time.getFullYear()}`;
         if (year.length === 1) {
-          year  = '0' + year;
+          year = '0' + year;
         }
 
         return `${date}-${month}-${year}.log`;
@@ -79,33 +80,41 @@ class LoggerFile {
         throw new Meteor.Error('[LoggerFile] [options.format] Must be a Function!');
       }
     } else {
-      this.options.format = (time, level, message, data, userId) => {
+      this.options.format = (time, level, message, _data, userId) => {
         let month = `${time.getMonth() + 1}`;
         if (month.length === 1) {
           month = '0' + month;
         }
-        let date  = `${time.getDate()}`;
+        let date = `${time.getDate()}`;
         if (date.length === 1) {
-          date  = '0' + date;
+          date = '0' + date;
         }
-        let year  = `${time.getFullYear()}`;
+        let year = `${time.getFullYear()}`;
         if (year.length === 1) {
-          year  = '0' + year;
+          year = '0' + year;
         }
         let hours = `${time.getHours()}`;
         if (hours.length === 1) {
           hours = '0' + hours;
         }
-        let mins  = `${time.getMinutes()}`;
+        let mins = `${time.getMinutes()}`;
         if (mins.length === 1) {
-          mins  = '0' + mins;
+          mins = '0' + mins;
         }
-        let sec   = `${time.getSeconds()}`;
+        let sec = `${time.getSeconds()}`;
         if (sec.length === 1) {
-          sec   = '0' + sec;
+          sec = '0' + sec;
         }
 
-        return `${date}-${month}-${year} ${hours}:${mins}:${sec} | [${level}] | Message: \"${message}\" | User: ${userId} | data: ${data}\r\n`;
+        let data = helpers.clone(_data);
+
+        try {
+          data = JSON.stringify(data);
+        } catch (stringifyError) {
+          // Something is off about data object
+        }
+
+        return `${date}-${month}-${year} ${hours}:${mins}:${sec} | [${level}] | Message: \"${message}\" | User: ${userId} | data: ${data}\n`;
       };
     }
 
@@ -118,36 +127,33 @@ class LoggerFile {
       this.options.path = Meteor.rootPath + ((process.env.NODE_ENV === 'development') ? `${nodePath.sep}static${nodePath.sep}logs` : `${nodePath.sep}assets${nodePath.sep}app${nodePath.sep}logs`);
     }
 
-    const pathRegExp  = new RegExp(`${nodePath.sep}$`);
-    this.options.path = this.options.path.replace(pathRegExp, '');
+    const pathRegExp = new RegExp(`${nodePath.sep}$`);
+    this.options.path = nodePath.resolve(this.options.path.replace(pathRegExp, ''));
 
-    fs.ensureDir(`${this.options.path}`, (EDError) => {
-      if (EDError) {
-        throw new Meteor.Error('[LoggerFile] [options.path] Error:', EDError);
+    fs.mkdir(this.options.path, { recursive: true }, (mkdError) => {
+      if (mkdError) {
+        throw new Meteor.Error('[LoggerFile] [options.path] Error:', mkdError);
       }
 
-      fs.writeFile(`${this.options.path}${nodePath.sep}test`, 'test', (WFError) => {
-        if (WFError) {
-          throw new Meteor.Error(`[LoggerFile] [options.path] ${this.options.path} is not writable!!!`, WFError);
+      fs.writeFile(`${this.options.path}${nodePath.sep}test`, 'test', (wfError) => {
+        if (wfError) {
+          throw new Meteor.Error(`[LoggerFile] [options.path] ${this.options.path} is not writable!!!`, wfError);
         }
-        fs.unlink(`${this.options.path}${nodePath.sep}test`, NOOP);
+        fs.unlink(`${this.options.path}${nodePath.sep}test`, noop);
       });
     });
 
-    this.logger.add('File', (level, message, data = null, userId) => {
+    this.logger.add('File', (level, message, data, userId) => {
       const time = new Date();
-      let _data  = null;
 
       if (data) {
-        _data = this.logger.antiCircular(helpers.clone(data));
-        if (helpers.isString(_data.stackTrace)) {
-          _data.stackTrace = _data.stackTrace.split(/\n|\\n|\r|\r\n/g);
+        if (helpers.isString(data.stackTrace)) {
+          data.stackTrace = data.stackTrace.split(/\n|\\n|\r|\r\n/g);
         }
-        _data = JSON.stringify(_data, false, 2);
       }
 
-      fs.appendFile(`${this.options.path}${nodePath.sep}${this.options.fileNameFormat(time)}`, this.options.format(time, level, message, _data, userId), NOOP);
-    }, NOOP, false, false);
+      fs.appendFile(`${this.options.path}${nodePath.sep}${this.options.fileNameFormat(time)}`, this.options.format(time, level, message, data, userId), noop);
+    }, noop, false, false);
   }
 
   enable(rule = {}) {
@@ -158,13 +164,13 @@ class LoggerFile {
       filter: Match.Optional([String])
     });
 
-    if (rule.enable == null) {
+    if (typeof rule.enable === 'undefined') {
       rule.enable = true;
     }
-    if (rule.client == null) {
+    if (typeof rule.client === 'undefined') {
       rule.client = true;
     }
-    if (rule.server == null) {
+    if (typeof rule.server === 'undefined') {
       rule.server = true;
     }
 
