8ef213be3eaf87d5e4b6ace8cf3380056485263f
dr.dimitru <ceo@veliov.com>
2022-06-01 08:07:07 +0300

    📦 v2.1.0
    __Critical changes:__
    - ⚠️ Requires `node@^12.12.0` and `meteor@^2.3.x`
    
    __Other changes:__
    - 👷‍♂️ Avoid using external packages, rely on build-in `fs` node-module
    - 👨‍💻 Minor overall codebase refactoring
    - 🤝 Compatibility with latest `meteor@2.7.2`

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
   name: 'ostrio:loggerfile',
-  version: '2.0.7',
+  version: '2.1.0',
   summary: 'Logging: Store application\'s logs into file (Server & Client support)',
   git: 'https://github.com/VeliovGroup/Meteor-logger-file',
   documentation: 'README.md'
 });
 
 Package.onUse((api) => {
-  api.versionsFrom('1.4');
-  api.use('ostrio:meteor-root@1.0.8', 'server');
-  api.use(['ecmascript', 'check', 'ostrio:logger@2.0.8'], ['client', 'server']);
+  api.versionsFrom('2.3');
+  api.use('ostrio:meteor-root@1.1.1', 'server');
+  api.use(['ecmascript', 'check', 'ostrio:logger@2.1.1'], ['client', 'server']);
   api.mainModule('client.js', 'client');
   api.mainModule('server.js', 'server');
 });
@@ -18,8 +18,4 @@ Package.onTest((api) => {
   api.use('tinytest');
   api.use(['ecmascript', 'underscore', 'ostrio:logger', 'ostrio:loggerfile']);
   api.addFiles('loggerfile-tests.js');
-});
-
-Npm.depends({
-  'fs-extra': '7.0.1'
 });
